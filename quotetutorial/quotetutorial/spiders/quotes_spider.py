130dbb1e112f41c2d924ee3f1b314a0234b26cc7
Piyush_07 <luitelpiyush95@gmail.com>
2023-12-09 07:17:38 +0545

    some changes made

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -2,7 +2,15 @@ import scrapy
 
 # this class inherits from Spider class that inherits from scrapy class
 class QuoteSpider(scrapy.Spider):
+    # name of our spider
     name='quotes'
-    start_url=[
+    # we can put mulitple website in this list below
+    start_urls=[
         'https://quotes.toscrape.com/'
     ]
+
+    def parse(self,response):
+        # response contains the source code of the website https://quotes.toscrape.com/ in this case
+        title=response.css('title').extract()
+        yield {'titletext':title}
+
