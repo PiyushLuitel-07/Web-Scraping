d2577b1dcb3f50450e4902f7ba06ceba8a0386ed
Piyush_07 <luitelpiyush95@gmail.com>
2023-12-09 15:04:08 +0545

    connection parameters for mySQL is made

--- quotetutorial/quotetutorial/pipelines.py
+++ quotetutorial/quotetutorial/pipelines.py
@@ -7,7 +7,18 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import mysql.connector
+
 
 class QuotetutorialPipeline:
-    def process_item(self, item, spider):
-        return item
+   
+    def __init__(self):
+      self.create_connection()
+      self.create_table()
+    def create_connection(self):
+      self.conn=mysql.connector.connect(
+         host='localhost',
+         user='root',
+         passw='9803474343',
+         database='myquotes'
+      )
