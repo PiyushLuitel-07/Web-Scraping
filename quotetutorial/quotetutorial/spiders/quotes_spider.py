96b3308dc9a38ae37050ece540a648763c0a3c48
Piyush_07 <luitelpiyush95@gmail.com>
2023-12-09 07:27:19 +0545

    title of the website is scraped successfully

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -11,6 +11,6 @@ class QuoteSpider(scrapy.Spider):
 
     def parse(self,response):
         # response contains the source code of the website https://quotes.toscrape.com/ in this case
-        title=response.css('title').extract()
+        title=response.css('title::text').extract()
         yield {'titletext':title}
 
